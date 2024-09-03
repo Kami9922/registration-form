36e963a91527cc437058a5788bdd9d1b2e07f873
Vitalya <vitalikt_22@mail.ru>
2024-09-03 23:14:58 +0400

    Ending main task

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import styles from "./form.module.css";
 
 const Form = () => {
+	const [errorEmail, setErrorEmail] = useState(null);
+	const [errorPassword, setPassword] = useState(null);
+	const [errorRepassword, setRepassword] = useState(null);
+	const [regButton, setRegButton] = useState(false);
+
 	const initialState = {
 		email: "",
 		password: "",
@@ -19,10 +24,6 @@ const Form = () => {
 		};
 	};
 
-	// const [email, setEmail] = useState("");
-	// const [password, setPassword] = useState("");
-	// const [repassword, setRepassword] = useState("");
-
 	const sendData = (dataForm) => {
 		console.log(dataForm);
 	};
@@ -36,25 +37,94 @@ const Form = () => {
 
 	const { email, password, repassword } = getState();
 
-	const onChange = ({ target }) => updateState(target.name, target.value);
+	const submitButtonRef = useRef(null);
+
+	let error = null;
+
+	const onEmailChange = ({ target }) => {
+		updateState(target.name, target.value);
+
+		if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(target.value)) {
+			error = "Введите корректный email";
+		}
+		if (target.value.length > 20) {
+			error = "Email не может содержать больше 20 символов";
+		}
+
+		setErrorEmail(error);
+	};
+	const onPassChange = ({ target }) => {
+		updateState(target.name, target.value);
+
+		if (!/^[\w_]*$/.test(target.value)) {
+			error = "Неверный пароль. Допустимые символы: буквы, цифры и нижнее подчёркивание";
+		}
+		if (target.value.length < 3) {
+			error = "В пароле не может быть меньше 3 символов";
+		}
+		if (target.value.length > 20) {
+			error = "В пароле не может быть больше 20 символов";
+		}
+
+		setPassword(error);
+	};
+
+	const onRepassChange = ({ target }) => {
+		updateState(target.name, target.value);
+
+		let error = null;
+
+		if (target.value !== password) {
+			error = "Пароли не совпадают";
+		}
+
+		setRepassword(error);
+
+		if (target.value.length === password.length) {
+			setTimeout(() => {
+				submitButtonRef.current?.focus();
+			}, 0);
+		}
+	};
+
+	const onClickButton = () => setRegButton(true);
 
 	return (
 		<div>
 			<form className={styles.form} onSubmit={onSubmit}>
-				{/* {(loginError || passError || rePassError) && (
-					<div>{loginError || passError || rePassError}</div>
-				)} */}
+				{regButton && (errorEmail || errorPassword || errorRepassword) && (
+					<div>{errorEmail || errorPassword || errorRepassword}</div>
+				)}
 				<span>Email</span>
-				<input name="email" type="email" value={email} onChange={onChange} />
+				<input
+					name="email"
+					type="text"
+					value={email}
+					onChange={onEmailChange}
+					placeholder="Введите email"
+				/>
 				<span>Пароль</span>
-				<input name="password" type="password" value={password} onChange={onChange} />
+				<input
+					name="password"
+					type="password"
+					value={password}
+					onChange={onPassChange}
+					placeholder="Введите пароль"
+				/>
 				<span>Повтор пароля</span>
-				<input name="repassword" type="password" value={repassword} onChange={onChange} />
+				<input
+					name="repassword"
+					type="password"
+					value={repassword}
+					onChange={onRepassChange}
+					placeholder="Повторите пароль"
+				/>
 				<div>
 					<button
 						type="submit"
-						// ref={submitButtonRef}
-						// disabled={loginError || passError || rePassError}
+						disabled={regButton && (errorEmail || errorPassword || errorRepassword)}
+						ref={submitButtonRef}
+						onClick={onClickButton}
 					>
 						Зарегистрироваться
 					</button>
